8c1d9c9b116fb57a10bb8a40d9f9c2a57af4a7e1
ending321 <93332070+ending321@users.noreply.github.com>
2025-07-29 23:54:52 +0800

    Add files via upload

---
+++ project2-microservices/web-service/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'appdb',
+        'USER': 'root',
+        'PASSWORD': 'chenjiewei',
+        'HOST': '192.168.31.17',
+        'PORT': '3306',
+    }
+}
